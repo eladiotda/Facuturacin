f605abd29fef98ace12ce3acc921946569c4f6da
eladiotda <eladio.lopez@correo.tdea.edu.co>
2026-05-08 11:02:44 -0500

    [Facturacion] Controlador de facturas con detalle y total

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -2,11 +2,13 @@
 
 from app.controllers.cliente_controller import ClienteController
 from app.controllers.exceptions import ConflictError, ValidationError
+from app.controllers.factura_controller import FacturaController
 from app.controllers.producto_controller import ProductoController
 
 __all__ = [
     "ClienteController",
     "ConflictError",
+    "FacturaController",
     "ProductoController",
     "ValidationError",
 ]
